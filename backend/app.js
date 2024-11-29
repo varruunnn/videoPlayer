ba2f278e80521c56c9de2ec2aaaf4739deef43fb
Varun Choudhary <143994029+varruunnn@users.noreply.github.com>
2024-11-29 15:32:45 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,14 +9,13 @@ require('dotenv').config()
 
 const PORT = process.env.PORT || 8000
 
-//middlewares
+
 app.use(cors())
 app.use(express.json())
 
-//routes
+
 readdirSync('./routes').map((route) => app.use('/api', require('./routes/' + route)))
 
-//serve static files
 app.use('/public', express.static(path.join(__dirname, 'public')))
 
 const server = () => {
